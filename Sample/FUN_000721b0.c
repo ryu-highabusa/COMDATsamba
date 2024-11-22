11c774980b7078ec816859c5b53c1f64197c3a4a
ryu-highabusa <98571310+ryu-highabusa@users.noreply.github.com>
2024-11-22 08:59:02 -0700

    Update FUN_000721b0.c

--- Sample/FUN_000721b0.c
+++ Sample/FUN_000721b0.c
@@ -1,5 +1,5 @@
 
-void FUN_000721b0(void)
+void ConfigureGameSettings_FUN_000721b0(void)
 
 {
   undefined uVar1;
@@ -22,8 +22,8 @@ void FUN_000721b0(void)
   case '\0':
     DAT_00596ad4 = 0;
     DAT_00596ad5 = 0;
-    HITPOINT_SETTING_CURRENT__ = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c4];
-    DAT_005a27e2 = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c4];
+    HitPoint_P1_005a27e0 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c4];
+    HitPoint_P2_005a27e2 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c4];
     DAT_00591fa4 = 3;
     DAT_00588837 = 1;
     return;
@@ -36,8 +36,8 @@ void FUN_000721b0(void)
     if (DAT_00596a74 != '\0') {
       DAT_00596aa4 = Settings_Game_RoundTime_0059d092;
       DAT_0059c5c8 = (&RoundTime_Values_00302f28)[Settings_Game_RoundTime_0059d092];
-      HITPOINT_SETTING_CURRENT__ = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c4];
-      DAT_005a27e2 = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c4];
+      HitPoint_P1_005a27e0 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c4];
+      HitPoint_P2_005a27e2 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c4];
       DAT_00596ad5 = Settings_Game_LifeGauge_0059d0c4 != 5;
       DAT_00596ad4 = Settings_Game_LifeGauge_0059d0c4 != 5;
       DAT_005b1d6c = 0;
@@ -46,8 +46,8 @@ void FUN_000721b0(void)
     }
     DAT_00591fa4 = Settings_Game_Difficulty_0059d088;
     DAT_0059c5c8 = RoundTime_Values_00302f2d;
-    HITPOINT_SETTING_CURRENT__ = HITPOINT_VALUES_00302f10;
-    DAT_005a27e2 = HITPOINT_VALUES_00302f10;
+    HitPoint_P1_005a27e0 = HitPoint_Values_00302f10;
+    HitPoint_P2_005a27e2 = HitPoint_Values_00302f10;
     DAT_00596aa4 = 5;
     DAT_00588837 = 1;
     DAT_005b1d6c = 1;
@@ -60,8 +60,8 @@ void FUN_000721b0(void)
     if (DAT_00596a74 != '\0') {
       DAT_00596aa4 = Settings_Game_RoundTime_0059d092;
       DAT_0059c5c8 = (&RoundTime_Values_00302f28)[Settings_Game_RoundTime_0059d092];
-      HITPOINT_SETTING_CURRENT__ = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c4];
-      DAT_005a27e2 = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c4];
+      HitPoint_P1_005a27e0 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c4];
+      HitPoint_P2_005a27e2 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c4];
       DAT_00596ad5 = Settings_Game_LifeGauge_0059d0c4 != 5;
       DAT_00596ad4 = Settings_Game_LifeGauge_0059d0c4 != 5;
       DAT_005b1d6c = 0;
@@ -72,8 +72,8 @@ void FUN_000721b0(void)
     if (DAT_00c59866 == '\0') {
       DAT_0059c5c8 = RoundTime_Values_00302f2b;
       DAT_00588837 = DAT_00302f31;
-      HITPOINT_SETTING_CURRENT__ = HITPOINT_VALUES_00302f0c;
-      DAT_005a27e2 = HITPOINT_VALUES_00302f0c;
+      HitPoint_P1_005a27e0 = HitPoint_Values_00302f0c;
+      HitPoint_P2_005a27e2 = HitPoint_Values_00302f0c;
       DAT_005b146a = WATCHMODE_FLAG__ != '\x01';
       DAT_005b1d6c = 0;
       DAT_00596aa4 = 3;
@@ -89,7 +89,7 @@ void FUN_000721b0(void)
       return;
     }
     DAT_0059c5c8 = RoundTime_Values_00302f2c;
-    DAT_005a27e2 = HITPOINT_VALUES_00302f0c;
+    HitPoint_P2_005a27e2 = HitPoint_Values_00302f0c;
     DAT_00588837 = DAT_00302f30;
     uVar6 = (uint)(WATCHMODE_FLAG__ != '\x01');
     DAT_00596aa4 = 4;
@@ -107,8 +107,8 @@ void FUN_000721b0(void)
     DAT_0059c5c8 = (&RoundTime_Values_00302f28)[Settings_Game_RoundTime_0059d092];
     DAT_0059d065 = 0;
     DAT_005a0ec8 = 0;
-    HITPOINT_SETTING_CURRENT__ = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c4];
-    DAT_005a27e2 = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c4];
+    HitPoint_P1_005a27e0 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c4];
+    HitPoint_P2_005a27e2 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c4];
     DAT_005b134a = 0;
     DAT_005b1d6c = 0;
     return;
@@ -118,8 +118,8 @@ void FUN_000721b0(void)
     if (DAT_00596a74 != '\0') {
       DAT_00596aa4 = Settings_Game_RoundTime_0059d092;
       DAT_0059c5c8 = (&RoundTime_Values_00302f28)[Settings_Game_RoundTime_0059d092];
-      HITPOINT_SETTING_CURRENT__ = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c4];
-      DAT_005a27e2 = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c4];
+      HitPoint_P1_005a27e0 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c4];
+      HitPoint_P2_005a27e2 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c4];
       DAT_00596ad5 = Settings_Game_LifeGauge_0059d0c4 != 5;
       DAT_00596ad4 = Settings_Game_LifeGauge_0059d0c4 != 5;
       DAT_005b1d6c = 0;
@@ -132,8 +132,8 @@ void FUN_000721b0(void)
     DAT_005b1d6c = 0;
     DAT_00596aa4 = (DAT_00c59866 != '\0') * '\x02' + 1;
     DAT_0059c5c8 = (&RoundTime_Values_00302f28)[DAT_00596aa4];
-    HITPOINT_SETTING_CURRENT__ = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c2];
-    DAT_005a27e2 = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c2];
+    HitPoint_P1_005a27e0 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c2];
+    HitPoint_P2_005a27e2 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c2];
     uVar6 = (uint)(WATCHMODE_FLAG__ != '\x01');
     DAT_005b146a = WATCHMODE_FLAG__ != '\x01';
     DAT_005b1468 = (&DAT_00588838)[uVar6 * 5];
@@ -142,8 +142,8 @@ void FUN_000721b0(void)
     return;
   case '\a':
   case '\v':
-    HITPOINT_SETTING_CURRENT__ = HITPOINT_VALUES_00302f10;
-    DAT_005a27e2 = HITPOINT_VALUES_00302f10;
+    HitPoint_P1_005a27e0 = HitPoint_Values_00302f10;
+    HitPoint_P2_005a27e2 = HitPoint_Values_00302f10;
     DAT_00591fa4 = 1;
     DAT_00588837 = 1;
     DAT_00596ad4 = 0;
@@ -159,8 +159,8 @@ void FUN_000721b0(void)
     DAT_00596aa4 = Settings_Game_RoundTime_0059d08e;
     if (DAT_00596a74 != '\0') {
       DAT_0059c5c8 = (&RoundTime_Values_00302f28)[Settings_Game_RoundTime_0059d08e];
-      HITPOINT_SETTING_CURRENT__ = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c1];
-      DAT_005a27e2 = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c1];
+      HitPoint_P1_005a27e0 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c1];
+      HitPoint_P2_005a27e2 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c1];
       DAT_00596ad5 = Settings_Game_LifeGauge_0059d0c1 != 5;
       DAT_00596ad4 = Settings_Game_LifeGauge_0059d0c1 != 5;
       DAT_005b1d6c = 0;
@@ -170,7 +170,7 @@ void FUN_000721b0(void)
     DAT_00591fa4 = Settings_Game_Difficulty_0059d08a;
     DAT_0059c5c8 = (&RoundTime_Values_00302f28)[Settings_Game_RoundTime_0059d08e];
     DAT_00596ad5 = Settings_Game_LifeGauge_0059d0c1 != 5;
-    DAT_005a27e2 = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c1];
+    HitPoint_P2_005a27e2 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c1];
     DAT_00588837 = (&DAT_00302f30)[Settings_Game_MatchPoint_0059d0c6];
     if (WATCHMODE_FLAG__ == '\x01') {
       puVar2 = &WATCHMODE_FLAG__;
@@ -194,8 +194,8 @@ void FUN_000721b0(void)
     DAT_00591fa4 = Settings_Game_Difficulty_0059d08c;
     DAT_00596aa4 = Settings_Game_RoundTime_0059d091;
     DAT_0059c5c8 = (&RoundTime_Values_00302f28)[Settings_Game_RoundTime_0059d091];
-    HITPOINT_SETTING_CURRENT__ = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c3];
-    DAT_005a27e2 = (&HITPOINT_VALUES_00302f08)[Settings_Game_LifeGauge_0059d0c3];
+    HitPoint_P1_005a27e0 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c3];
+    HitPoint_P2_005a27e2 = (&HitPoint_Values_00302f08)[Settings_Game_LifeGauge_0059d0c3];
     DAT_00596ad5 = Settings_Game_LifeGauge_0059d0c3 != 5;
     DAT_00596ad4 = Settings_Game_LifeGauge_0059d0c3 != 5;
     DAT_0059cf44 = DAT_00599881;
@@ -207,8 +207,8 @@ void FUN_000721b0(void)
     return;
   case '\f':
   case '\r':
-    HITPOINT_SETTING_CURRENT__ = 0xf0;
-    DAT_005a27e2 = 0xf0;
+    HitPoint_P1_005a27e0 = 0xf0;
+    HitPoint_P2_005a27e2 = 0xf0;
     DAT_00596aa4 = 0;
     DAT_005b1d6c = 0;
     DAT_00596ad4 = 1;
@@ -220,7 +220,7 @@ void FUN_000721b0(void)
     return;
   }
   DAT_00596ad4 = DAT_00596ad5;
-  HITPOINT_SETTING_CURRENT__ = DAT_005a27e2;
+  HitPoint_P1_005a27e0 = HitPoint_P2_005a27e2;
   puVar2[3] = puVar2[3] & 1;
   puVar2[0x66] = puVar2[3];
   uVar1 = (&DAT_005a2620)[uVar6 * 5];
