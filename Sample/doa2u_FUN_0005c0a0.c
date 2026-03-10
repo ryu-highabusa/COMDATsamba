66c15308ff50138c4a5145b6b84e8c070a192d69
ryu-highabusa <98571310+ryu-highabusa@users.noreply.github.com>
2026-03-10 17:09:07 -0600

    .

---
+++ Sample/doa2u_FUN_0005c0a0.c
@@ -0,0 +1,47 @@
+
+/* WARNING: Globals starting with '_' overlap smaller symbols at the same address */
+
+void FUN_0005c0a0(void)
+
+{
+  int iVar1;
+  undefined4 *puVar2;
+  
+  if (DWORD_0084c24c == 0) {
+    DWORD_0084c24c = 1;
+    DAT_0084c0d0._0_2_ = 1;
+    puVar2 = (undefined4 *)&DAT_0084c1e8;
+    for (iVar1 = 0x10; iVar1 != 0; iVar1 = iVar1 + -1) {
+      *puVar2 = 0;
+      puVar2 = puVar2 + 1;
+    }
+    puVar2 = &DAT_0084c0d8;
+    for (iVar1 = 0x44; iVar1 != 0; iVar1 = iVar1 + -1) {
+      *puVar2 = 0;
+      puVar2 = puVar2 + 1;
+    }
+    _DAT_0084c0b0 = 0;
+    _DAT_0084c0b4 = 0;
+    _DAT_0084c0b8 = 0;
+    _DAT_0084c0bc = 0;
+    DWORD_0084c22c = 0;
+    DWORD_0084c230 = 0;
+    DWORD_0084c234 = 0;
+    DWORD_0084c238 = 0;
+    _DAT_0084c0c4 = 0;
+    _DAT_0084c0cc = 0;
+    DWORD_0084c244 = 0;
+    _DAT_0084c0c8 = 0;
+    DWORD_0084c248 = 0;
+    DWORD_0084c240 = 0;
+    DWORD_0084c23c = 0;
+    DWORD_0084c0dc = (dword)&SKELETON_DELETED;
+    DWORD_0084c120 = (dword)&SKELETON_DELETED;
+    DWORD_0084c164 = (dword)&SKELETON_DELETED;
+    DWORD_0084c1a8 = (dword)&SKELETON_DELETED;
+    FUN_00062130();
+    return;
+  }
+  return;
+}
+
