566314b2baec9728c630f769a55fab281994d659
ryu-highabusa <98571310+ryu-highabusa@users.noreply.github.com>
2026-09-07 13:10:09 -0600

    Create aliases.d.ts

---
+++ Sample/doa5lr_moveset_AlexXsWx_code_and_documentation/src/parsers/movesetBin/aliases.d.ts
@@ -0,0 +1,14 @@
+export type VarName = string;
+export type VarPath = Array<VarName>;
+
+export type ErrorMessage = string;
+
+export type AobStr = string;
+export type FlatAobStr = string;
+export type AobMatchStr = string;
+export type NumberAsAobStr = AobStr;
+export type NumberAsX_HexStr = string;
+
+// TODO: move somewhere else
+export type HslTypeName = string;
+export type HslStructureName = HslTypeName;
