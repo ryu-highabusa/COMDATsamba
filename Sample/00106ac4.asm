8399da8b7d452d6dcc99ab8e4ff9a949a0560389
ryu-highabusa <98571310+ryu-highabusa@users.noreply.github.com>
2026-09-07 12:29:56 -0600

    Update 00106ac4.asm

--- Sample/00106ac4.asm
+++ Sample/00106ac4.asm
@@ -1,5 +1,7 @@
-00106ac4 83 f8 01        CMP        EAX,0x1              ; Compare EAX (index) with 1
-00106ac7 0f 95 c1        SETNZ      CL                  ; Set CL to 1 if not zero (EAX != 1)
-00106aca 49              DEC        ECX                 ; Decrement ECX
-00106acb 83 e1 02        AND        ECX,0x2             ; Keep only the second bit of ECX
-00106ace 89 0c 85        MOV        dword ptr [EAX*0x4 + DAT_00d802f8],ECX ; Write ECX to table
+00106ac4 b9 01 00 00 00  MOV        ECX,0x1               ; Set ECX to 1
+00106ac9 89 0c 85        MOV        dword ptr [EAX*0x4 + DAT_00d802f8],ECX ; Write 1 to table
+                 f8 02 d8 00
+00106ad5 40              INC        EAX                   ; Increment EAX (loop counter)
+00106ad6 83 f8 15        CMP        EAX,0x15              ; Compare EAX with 21 (end of table)
+00106ad9 72 e7           JC         LAB_00106ac2          ; Jump back if EAX < 21
+00106adb c3              RET                              ; Return
