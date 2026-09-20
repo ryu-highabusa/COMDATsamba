3461df5e15350652dcdb0010ae49a04f04905c16
ryu-highabusa <98571310+ryu-highabusa@users.noreply.github.com>
2026-09-20 14:02:51 -0600

    Create FUN_0003ecc0_1996doa.c

---
+++ Sample/FUN_0003ecc0_1996doa.c
@@ -0,0 +1,176 @@
+
+/* WARNING: Globals starting with '_' overlap smaller symbols at the same address */
+/* WARNING: Restarted to delay deadcode elimination for space: register */
+
+undefined1
+FUN_0003ecc0(undefined4 param_1,undefined4 param_2,undefined4 param_3,undefined4 param_4,
+            undefined4 param_5,undefined4 param_6,undefined4 param_7,undefined4 param_8,
+            undefined4 param_9,undefined4 param_10,undefined4 param_11,undefined4 param_12)
+
+{
+  undefined4 unaff_pfp;
+  undefined1 in_register_00000008 [56];
+  undefined1 auVar2 [64];
+  undefined1 auVar3 [64];
+  undefined1 auVar4 [64];
+  undefined1 auVar5 [64];
+  undefined1 auVar6 [64];
+  undefined1 auVar7 [64];
+  undefined1 auVar8 [64];
+  undefined1 auVar9 [64];
+  undefined1 auVar10 [64];
+  undefined1 auVar11 [64];
+  undefined1 auVar12 [64];
+  undefined1 auVar13 [64];
+  undefined1 auVar14 [64];
+  undefined4 uVar15;
+  undefined4 extraout_g1;
+  undefined4 extraout_g1_00;
+  undefined4 extraout_g1_01;
+  undefined4 extraout_g1_02;
+  undefined4 extraout_g1_03;
+  undefined4 extraout_g1_04;
+  undefined4 extraout_g1_05;
+  undefined4 extraout_g1_06;
+  undefined4 extraout_g1_07;
+  undefined4 extraout_g1_08;
+  undefined4 extraout_g1_09;
+  undefined4 extraout_g1_10;
+  undefined4 extraout_g1_11;
+  undefined4 extraout_g1_12;
+  undefined8 uVar16;
+  undefined1 auStackX_0 [64];
+  undefined1 auStack_40 [64];
+  undefined1 auStack_80 [64];
+  undefined1 auStack_c0 [64];
+  undefined1 auStack_100 [64];
+  undefined1 auStack_140 [64];
+  undefined1 auStack_180 [64];
+  undefined1 auStack_1c0 [64];
+  undefined1 auStack_200 [64];
+  undefined1 auStack_240 [64];
+  undefined1 auStack_280 [64];
+  undefined1 auStack_2c0 [64];
+  undefined1 auStack_300 [64];
+  undefined1 auStack_340 [999168];
+  undefined1 auVar1 [64];
+  
+  auVar2._4_4_ = auStackX_0;
+  auVar2._0_4_ = unaff_pfp;
+  uVar15 = 0xf;
+  auVar2._12_52_ = in_register_00000008._4_52_;
+  auVar2._8_4_ = 0x3eccc;
+  *(undefined1 (*) [64])((uint)fp & 0xffffffc0) = auVar2;
+  auVar1._8_56_ = auVar2._8_56_;
+  auVar1._4_4_ = auStackX_0;
+  auVar1._0_4_ = fp;
+  Debug_SetTextPosition(8,0xf);
+  auStackX_0._12_52_ = auVar1._12_52_;
+  auStackX_0._0_8_ = auVar1._0_8_;
+  auStackX_0._8_4_ = 0x3ecd8;
+  auVar3._8_56_ = auStackX_0._8_56_;
+  auVar3._4_4_ = auStack_40;
+  auVar3._0_4_ = auStackX_0;
+  FUN_0008e740(s_CHEST_0003ec48,uVar15,param_3,param_4,param_5,param_6,param_7,param_8,param_9,
+               param_10,param_11,param_12);
+  auStack_40._12_52_ = auVar3._12_52_;
+  auStack_40._0_8_ = auVar3._0_8_;
+  auStack_40._8_4_ = 0x3ece4;
+  auVar4._8_56_ = auStack_40._8_56_;
+  auVar4._4_4_ = auStack_80;
+  auVar4._0_4_ = auStack_40;
+  FUN_0008e740(s_HEAD_0003ec50,extraout_g1,param_3,param_4,param_5,param_6,param_7,param_8,param_9,
+               param_10,param_11,param_12);
+  auStack_80._12_52_ = auVar4._12_52_;
+  auStack_80._0_8_ = auVar4._0_8_;
+  auStack_80._8_4_ = 0x3ecf0;
+  auVar5._8_56_ = auStack_80._8_56_;
+  auVar5._4_4_ = auStack_c0;
+  auVar5._0_4_ = auStack_80;
+  FUN_0008e740(s_HIPS_0003ec58,extraout_g1_00,param_3,param_4,param_5,param_6,param_7,param_8,
+               param_9,param_10,param_11,param_12);
+  auStack_c0._12_52_ = auVar5._12_52_;
+  auStack_c0._0_8_ = auVar5._0_8_;
+  auStack_c0._8_4_ = 0x3ecfc;
+  auVar6._8_56_ = auStack_c0._8_56_;
+  auVar6._4_4_ = auStack_100;
+  auVar6._0_4_ = auStack_c0;
+  FUN_0008e740(s_LFOOT_0003ec60,extraout_g1_01,param_3,param_4,param_5,param_6,param_7,param_8,
+               param_9,param_10,param_11,param_12);
+  auStack_100._12_52_ = auVar6._12_52_;
+  auStack_100._0_8_ = auVar6._0_8_;
+  auStack_100._8_4_ = 0x3ed08;
+  auVar7._8_56_ = auStack_100._8_56_;
+  auVar7._4_4_ = auStack_140;
+  auVar7._0_4_ = auStack_100;
+  FUN_0008e740(s_LFORE_0003ec68,extraout_g1_02,param_3,param_4,param_5,param_6,param_7,param_8,
+               param_9,param_10,param_11,param_12);
+  auStack_140._12_52_ = auVar7._12_52_;
+  auStack_140._0_8_ = auVar7._0_8_;
+  auStack_140._8_4_ = 0x3ed14;
+  auVar8._8_56_ = auStack_140._8_56_;
+  auVar8._4_4_ = auStack_180;
+  auVar8._0_4_ = auStack_140;
+  FUN_0008e740(s_LHAND_0003ec70,extraout_g1_03,param_3,param_4,param_5,param_6,param_7,param_8,
+               param_9,param_10,param_11,param_12);
+  auStack_180._12_52_ = auVar8._12_52_;
+  auStack_180._0_8_ = auVar8._0_8_;
+  auStack_180._8_4_ = 0x3ed20;
+  auVar9._8_56_ = auStack_180._8_56_;
+  auVar9._4_4_ = auStack_1c0;
+  auVar9._0_4_ = auStack_180;
+  FUN_0008e740(s_LHIP_0003ec78,extraout_g1_04,param_3,param_4,param_5,param_6,param_7,param_8,
+               param_9,param_10,param_11,param_12);
+  auStack_1c0._12_52_ = auVar9._12_52_;
+  auStack_1c0._0_8_ = auVar9._0_8_;
+  auStack_1c0._8_4_ = 0x3ed2c;
+  auVar10._8_56_ = auStack_1c0._8_56_;
+  auVar10._4_4_ = auStack_200;
+  auVar10._0_4_ = auStack_1c0;
+  FUN_0008e740(s_LSHIN_0003ec80,extraout_g1_05,param_3,param_4,param_5,param_6,param_7,param_8,
+               param_9,param_10,param_11,param_12);
+  auStack_200._12_52_ = auVar10._12_52_;
+  auStack_200._0_8_ = auVar10._0_8_;
+  auStack_200._8_4_ = 0x3ed38;
+  auVar11._8_56_ = auStack_200._8_56_;
+  auVar11._4_4_ = auStack_240;
+  auVar11._0_4_ = auStack_200;
+  FUN_0008e740(s_LSHLD_0003ec88,extraout_g1_06,param_3,param_4,param_5,param_6,param_7,param_8,
+               param_9,param_10,param_11,param_12);
+  auStack_240._12_52_ = auVar11._12_52_;
+  auStack_240._0_8_ = auVar11._0_8_;
+  auStack_240._8_4_ = 0x3ed44;
+  auVar12._8_56_ = auStack_240._8_56_;
+  auVar12._4_4_ = auStack_280;
+  auVar12._0_4_ = auStack_240;
+  FUN_0008e740(s_RFOOT_0003ec90,extraout_g1_07,param_3,param_4,param_5,param_6,param_7,param_8,
+               param_9,param_10,param_11,param_12);
+  auStack_280._12_52_ = auVar12._12_52_;
+  auStack_280._0_8_ = auVar12._0_8_;
+  auStack_280._8_4_ = 0x3ed50;
+  auVar13._8_56_ = auStack_280._8_56_;
+  auVar13._4_4_ = auStack_2c0;
+  auVar13._0_4_ = auStack_280;
+  FUN_0008e740(s_RFORE_0003ec98,extraout_g1_08,param_3,param_4,param_5,param_6,param_7,param_8,
+               param_9,param_10,param_11,param_12);
+  auStack_2c0._12_52_ = auVar13._12_52_;
+  auStack_2c0._0_8_ = auVar13._0_8_;
+  auStack_2c0._8_4_ = 0x3ed5c;
+  auVar14._8_56_ = auStack_2c0._8_56_;
+  auVar14._4_4_ = auStack_300;
+  auVar14._0_4_ = auStack_2c0;
+  FUN_0008e740(s_RHAND_0003eca0,extraout_g1_09,param_3,param_4,param_5,param_6,param_7,param_8,
+               param_9,param_10,param_11,param_12);
+  auStack_300._12_52_ = auVar14._12_52_;
+  auStack_300._0_8_ = auVar14._0_8_;
+  auStack_300._8_4_ = 0x3ed68;
+  FUN_0008e740(s_RHIP_0003eca8,extraout_g1_10,param_3,param_4,param_5,param_6,param_7,param_8,
+               param_9,param_10,param_11,param_12);
+  FUN_0008e740(s_RSHIN_0003ecb0,extraout_g1_11,param_3,param_4,param_5,param_6,param_7,param_8,
+               param_9,param_10,param_11,param_12);
+  uVar16 = FUN_0008e740(s_RSHLD_0003ecb8,extraout_g1_12,param_3,param_4,param_5,param_6,param_7,
+                        param_8,param_9,param_10,param_11,param_12);
+  fp = auStack_340;
+  return (&DAT_00554bb0)[(int)((ulonglong)uVar16 >> 0x20) * 4 + (int)uVar16 * 0x3c];
+}
+
