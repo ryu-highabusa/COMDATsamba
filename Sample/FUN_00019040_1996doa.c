4dcca5c0e205bc602bbe39bf6453cb15db636ae9
ryu-highabusa <98571310+ryu-highabusa@users.noreply.github.com>
2026-09-24 04:49:00 -0600

    Update FUN_00019040_1996doa.c

--- Sample/FUN_00019040_1996doa.c
+++ Sample/FUN_00019040_1996doa.c
@@ -5,21 +5,33 @@
 void FUN_00019040(void)
 
 {
-  undefined1 *unaff_pfp;
+  undefined4 unaff_pfp;
   undefined1 in_register_0000000c [52];
-  undefined1 auVar1 [64];
+  undefined1 auVar2 [64];
+  undefined1 auVar3 [64];
   undefined1 auStackX_0 [1000000];
+  undefined1 auVar1 [64];
+  undefined1 auVar4 [64];
   
-  auVar1._4_4_ = auStackX_0;
-  auVar1._0_4_ = unaff_pfp;
+  auVar2._4_4_ = auStackX_0;
+  auVar2._0_4_ = unaff_pfp;
   ac = ac & 0xfffffff8 | (uint)(DAT_00557c61 != '\0') << 2 | (uint)(DAT_00557c61 == '\0') << 1;
   if (DAT_00557c61 == '\0') {
-    auVar1._8_4_ = 0x1905c;
-    auVar1._12_52_ = in_register_0000000c;
-    *(undefined1 (*) [64])((uint)fp & 0xffffffc0) = auVar1;
-    FUN_00008220(&LAB_000646f0,8);
-    unaff_pfp = (undefined1 *)register0x00000004;
-    FUN_00008220(&LAB_00064710,10);
+    auVar2._8_4_ = 0x1905c;
+    auVar2._12_52_ = in_register_0000000c;
+    *(undefined1 (*) [64])(fp & 0xffffffc0) = auVar2;
+    auVar1._8_56_ = auVar2._8_56_;
+    auVar1._4_4_ = auStackX_0;
+    auVar1._0_4_ = fp;
+    Task_RegisterOrReplace(&LAB_000646f0,8);
+    auVar4._12_52_ = auVar1._12_52_;
+    auVar4._0_8_ = auVar1._0_8_;
+    auVar4._8_4_ = 0x1906c;
+    auVar3._8_56_ = auVar4._8_56_;
+    auVar3._4_4_ = 0;
+    auVar3._0_4_ = auStackX_0;
+    Task_RegisterOrReplace(&LAB_00064710,10);
+    unaff_pfp = auVar3._0_4_;
     DAT_00557c61 = '\x01';
   }
   fp = unaff_pfp;
