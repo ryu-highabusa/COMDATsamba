07609d5b876eb5e7bb1269d8b4d4e69f64ac1512
ryu-highabusa <98571310+ryu-highabusa@users.noreply.github.com>
2026-09-24 17:44:10 -0600

    Update FUN_00024d50_1996doa.c

--- Sample/FUN_00024d50_1996doa.c
+++ Sample/FUN_00024d50_1996doa.c
@@ -1,5 +1,5 @@
 
-void FUN_00024d50(uint param_1)
+void Player_ApplyActCodeCombatMetadatamaybe(uint param_1)
 
 {
   byte bVar1;
@@ -10,7 +10,7 @@ void FUN_00024d50(uint param_1)
   undefined *puVar5;
   
   uVar3 = ac;
-  if ((&g_player1)[param_1].action_state == '\x05') {
+  if ((&g_player1)[param_1].action_state == STATE_HOLD) {
     bVar2 = (&g_player1)[param_1].action_flag;
     ac = ac & 0xfffffff8 | (uint)(1 < bVar2) << 2 | (uint)(bVar2 == 1) << 1 | (uint)(bVar2 == 0);
     uVar3 = param_1;
@@ -18,24 +18,24 @@ void FUN_00024d50(uint param_1)
       uVar3 = param_1 ^ 1;
     }
     (&g_player1)[param_1].attack_height =
-         (&PTR_DAT_0009c670_ZACK_0009c630)[(&g_player1)[param_1].character_id]
-         [(uint)(byte)(&g_character_act_record_table_candidate)[(&g_player1)[uVar3].character_id]
+         g_hold_metadata_table_by_character[(&g_player1)[param_1].character_id]
+         [(uint)(byte)g_character_act_record_tables[(&g_player1)[uVar3].character_id]
                       [(uint)(&g_player1)[param_1].action_code * 8 + 4] * 0xc + 0xb];
     (&g_player1)[param_1].attack_point = (DOA_U8)g14;
     (&g_player1)[param_1].attack_direction = (&g_player1)[param_1].facing_direction;
     fp = unaff_pfp;
     return;
   }
-  bVar2 = (&g_player1)[param_1].action_state - 2;
+  bVar2 = (&g_player1)[param_1].action_state - STATE_JUMPATTACK;
   ac = ac & 0xfffffff8 | (uint)(1 < bVar2) << 2 | (uint)(bVar2 == 1) << 1 |
-       (uint)((&g_player1)[param_1].action_state == '\x02');
+       (uint)((&g_player1)[param_1].action_state == STATE_JUMPATTACK);
   if (1 >= bVar2) {
-    puVar5 = (&PTR_DAT_0009d900_ZACK_0009d8c0)[(&g_player1)[param_1].character_id];
+    puVar5 = g_attack_metadata_table_by_character[(&g_player1)[param_1].character_id];
     uVar4 = param_1;
     if ((&g_player1)[param_1].action_flag == '\x01') {
       uVar4 = param_1 ^ 1;
     }
-    bVar2 = (&g_character_act_record_table_candidate)[(&g_player1)[uVar4].character_id]
+    bVar2 = g_character_act_record_tables[(&g_player1)[uVar4].character_id]
             [(uint)(&g_player1)[param_1].action_code * 8 + 4];
     bVar1 = puVar5[(uint)bVar2 * 0x14 + 0xb];
     (&g_player1)[param_1].attack_point = bVar1 / 4;
