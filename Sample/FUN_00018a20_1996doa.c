dbe6c3c0ef322132dfce36c63f9b27418af7950e
ryu-highabusa <98571310+ryu-highabusa@users.noreply.github.com>
2026-09-24 08:07:49 -0600

    Update FUN_00018a20_1996doa.c

--- Sample/FUN_00018a20_1996doa.c
+++ Sample/FUN_00018a20_1996doa.c
@@ -24,6 +24,7 @@ void FUN_00018a20(void)
   g_player1.pendingDamage = g14._0_2_;
   g_player1.lastHitDamage = g14._0_2_;
   g_player1.damageDisplayAmount = g14._0_2_;
+  g_player1.action_code = g14._0_1_;
   g_player1.action_flag = g14._0_1_;
   g_player1.action_request = g14._0_1_;
   g_player1.action_state = g14._0_1_;
@@ -34,7 +35,7 @@ void FUN_00018a20(void)
   g_player1.attack_point = g14._0_1_;
   g_player1.attack_state = g14._0_1_;
   g_player1.animation_flip = g14._0_1_;
-  g_player1.animation_flag = g14._0_1_;
+  g_player1.animation_request = g14._0_1_;
   g_player1.hit_attack = g14._0_1_;
   g_player1.hit_body = g14._0_1_;
   g_player1.hit_stage = g14._0_1_;
@@ -52,12 +53,12 @@ void FUN_00018a20(void)
   g_player1.ring_out = g14._0_1_;
   g_player1.attack_height = g14._0_1_;
   g_player1.ring_hit = g14._0_1_;
-  g_player1.player_display = g14._0_1_;
-  g_player1.danger_flag = '\x01';
+  g_player1.player_display = '\x01';
+  g_player1.danger_flag = g14._0_1_;
   g_player1.direction_adjust = g14._0_1_;
   g_player1.damage_number = g14._0_1_;
   g_player1.side_spin = g14._0_1_;
-  g_player1.hit_grasp = g14._0_1_;
+  g_player1.grapple_slip = g14._0_1_;
   g_player1.beat_hit_state = g14._0_1_;
   g_player1.ukemi_flag = g14._0_1_;
   g_player1.danger_set = g14._0_1_;
@@ -65,7 +66,6 @@ void FUN_00018a20(void)
   g_player1.combo_count = g14._0_1_;
   g_player1.combo_start = g14._0_1_;
   g_player1.cancel_use = g14._0_1_;
-  g_player1.unknown_56[0] = g14._0_1_;
   ac = ac & 0xfffffff8 | (uint)(NAME_RAIDOU < g_player2.character_id) << 2 |
        (uint)(g_player2.character_id == NAME_RAIDOU) << 1 |
        (uint)(g_player2.character_id < NAME_RAIDOU);
@@ -95,6 +95,7 @@ LAB_00018c64:
   g_player2.currentHealth = (ushort)HIT_POINT_CurrentSetting_005555e6;
   g_player2.lastHitDamage = g_player2.pendingDamage;
   g_player2.damageDisplayAmount = g_player2.pendingDamage;
+  g_player2.action_code = g14._0_1_;
   g_player2.action_flag = g14._0_1_;
   g_player2.action_request = g14._0_1_;
   g_player2.action_state = g14._0_1_;
@@ -105,7 +106,7 @@ LAB_00018c64:
   g_player2.attack_point = g14._0_1_;
   g_player2.attack_state = g14._0_1_;
   g_player2.animation_flip = g14._0_1_;
-  g_player2.animation_flag = g14._0_1_;
+  g_player2.animation_request = g14._0_1_;
   g_player2.hit_attack = g14._0_1_;
   g_player2.hit_body = g14._0_1_;
   g_player2.hit_stage = g14._0_1_;
@@ -123,12 +124,12 @@ LAB_00018c64:
   g_player2.ring_out = g14._0_1_;
   g_player2.attack_height = g14._0_1_;
   g_player2.ring_hit = g14._0_1_;
-  g_player2.player_display = g14._0_1_;
-  g_player2.danger_flag = '\x01';
+  g_player2.player_display = '\x01';
+  g_player2.danger_flag = g14._0_1_;
   g_player2.direction_adjust = g14._0_1_;
   g_player2.damage_number = g14._0_1_;
   g_player2.side_spin = g14._0_1_;
-  g_player2.hit_grasp = g14._0_1_;
+  g_player2.grapple_slip = g14._0_1_;
   g_player2.beat_hit_state = g14._0_1_;
   g_player2.ukemi_flag = g14._0_1_;
   g_player2.danger_set = g14._0_1_;
@@ -136,7 +137,6 @@ LAB_00018c64:
   g_player2.combo_count = g14._0_1_;
   g_player2.combo_start = g14._0_1_;
   g_player2.cancel_use = g14._0_1_;
-  g_player2.unknown_56[0] = g14._0_1_;
   fp = unaff_pfp;
   return;
 }
