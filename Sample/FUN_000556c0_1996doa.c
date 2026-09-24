76cba054410046f77b2afa9a41b56a07bbd26161
ryu-highabusa <98571310+ryu-highabusa@users.noreply.github.com>
2026-09-24 09:16:11 -0600

    Create FUN_000556c0_1996doa.c

---
+++ Sample/FUN_000556c0_1996doa.c
@@ -0,0 +1,16 @@
+
+void FUN_000556c0(void)
+
+{
+  undefined4 unaff_pfp;
+  
+  DAT_00588f20 = 0;
+  DAT_00588f24 = 0;
+  DAT_00588f28 = 0;
+  DAT_00588f2c = 0;
+  DAT_00588f30 = 0;
+  DAT_00588f34 = 0;
+  fp = unaff_pfp;
+  return;
+}
+
