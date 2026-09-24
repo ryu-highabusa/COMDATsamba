238c4abbb89766525cbe6a7ce672be1484bb5379
ryu-highabusa <98571310+ryu-highabusa@users.noreply.github.com>
2026-09-24 06:01:31 -0600

    Create FUN_00004550_1996doa.c

---
+++ Sample/FUN_00004550_1996doa.c
@@ -0,0 +1,16 @@
+
+void FUN_00004550(void)
+
+{
+  undefined4 unaff_pfp;
+  
+  DAT_0054fceb = g14;
+  DAT_0054fd8d = g14;
+  TimeTotal_Minutes_0054fd15 = g14;
+  TimeTotal_Seconds_0054fd16 = g14;
+  TimeTotal_MilliSeconds_0054fd17 = g14;
+  DAT_0054fd10 = g14;
+  fp = unaff_pfp;
+  return;
+}
+
