24af8e59301540487d99dbb92f770fe82ee9a92b
ryu-highabusa <98571310+ryu-highabusa@users.noreply.github.com>
2026-09-25 18:02:44 -0600

    Update FUN_00025b20_1996doa.c

--- Sample/FUN_00025b20_1996doa.c
+++ Sample/FUN_00025b20_1996doa.c
@@ -15,9 +15,9 @@ undefined4 FUN_00025b20(uint param_1)
   if ((&g_player1)[param_1].action_flag == '\x01') {
     uVar6 = param_1 ^ 1;
   }
-  if (1 < (byte)((&g_player1)[param_1].action_state - STATE_BLOCKSTUN)) {
-    if (((&g_player1)[param_1].action_state != STATE_HITSTUN) &&
-       ((&g_player1)[param_1].action_state != STATE_DOWNATTACKSTUN)) {
+  if (1 < (byte)((&g_player1)[param_1].action_state - 0x7_BLOCKSTUN)) {
+    if (((&g_player1)[param_1].action_state != 0x9_HITSTUN) &&
+       ((&g_player1)[param_1].action_state != 0xF_DOWNATTACKSTUN)) {
       bVar2 = g_move_def_table_by_character[(&g_player1)[uVar6].character_id]
               [(uint)(&g_player1)[param_1].action_code * 8 + 2];
       goto LAB_00025c50;
@@ -36,9 +36,8 @@ undefined4 FUN_00025b20(uint param_1)
   bVar2 = (byte)pwVar4[1];
 LAB_00025c50:
   DVar1 = DOA_ACTION_TRANSITION_ARRAY_000956f0[bVar2].next_state;
-  uVar6 = ac & 0xfffffff8 | (uint)(DVar1 != STATE_NEUTRAL) << 2 |
-          (uint)(DVar1 == STATE_NEUTRAL) << 1;
-  if (DVar1 == STATE_NEUTRAL) {
+  uVar6 = ac & 0xfffffff8 | (uint)(DVar1 != 0x0_NEUTRAL) << 2 | (uint)(DVar1 == 0x0_NEUTRAL) << 1;
+  if (DVar1 == 0x0_NEUTRAL) {
     bVar2 = DOA_ACTION_TRANSITION_ARRAY_000956f0[bVar2].next_action_code;
     uVar6 = ac & 0xfffffff8 | (uint)(bVar2 != 0) << 2 | (uint)(bVar2 == 0) << 1;
     if ((((byte)(uVar6 >> 1) & 1) == 1) ||
