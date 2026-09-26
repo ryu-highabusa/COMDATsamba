ef44ec5ae864070e6fbab40d1117c3922f4fb482
ryu-highabusa <98571310+ryu-highabusa@users.noreply.github.com>
2026-09-26 00:05:26 -0600

    Update FUN_00062f90_1996doa.c

--- Sample/FUN_00062f90_1996doa.c
+++ Sample/FUN_00062f90_1996doa.c
@@ -1,7 +1,7 @@
 
-undefined4
-FUN_00062f90(int param_1,undefined4 param_2,undefined4 param_3,undefined4 param_4,undefined4 param_5
-            )
+uint32_t SelectPlayerResourceVariant
+                   (uint32_t player_index,uint32_t p1_set0,uint32_t p2_set0,uint32_t p1_set1,
+                   uint32_t p2_set1)
 
 {
   uint uVar1;
@@ -10,19 +10,19 @@ FUN_00062f90(int param_1,undefined4 param_2,undefined4 param_3,undefined4 param_
   int iVar3;
   
   uVar2 = ac;
-  iVar3 = param_1 + (uint)(byte)(&DAT_005a6f1c)[param_1] * 3;
+  iVar3 = player_index + (uint)(byte)(&DAT_005a6f1c)[player_index] * 3;
   uVar1 = ac & 0xfffffff8 | (uint)(1 < iVar3) << 2 | (uint)(iVar3 == 1) << 1;
   ac = uVar1 | iVar3 < 1;
   if (((byte)(uVar1 >> 1) & 1) == 1) {
     fp = unaff_pfp;
-    return param_3;
+    return p2_set0;
   }
   if (iVar3 < 2) {
     uVar1 = uVar2 & 0xfffffff8 | (uint)(0 < iVar3) << 2 | (uint)(iVar3 == 0) << 1;
     ac = uVar1 | iVar3 < 0;
     if (((byte)(uVar1 >> 1) & 1) == 1) {
       fp = unaff_pfp;
-      return param_2;
+      return p1_set0;
     }
   }
   else {
@@ -30,13 +30,13 @@ FUN_00062f90(int param_1,undefined4 param_2,undefined4 param_3,undefined4 param_
     ac = uVar1 | iVar3 < 3;
     if (((byte)(uVar1 >> 1) & 1) == 1) {
       fp = unaff_pfp;
-      return param_4;
+      return p1_set1;
     }
     uVar1 = uVar2 & 0xfffffff8 | (uint)(4 < iVar3) << 2 | (uint)(iVar3 == 4) << 1;
     ac = uVar1 | iVar3 < 4;
     if (((byte)(uVar1 >> 1) & 1) == 1) {
       fp = unaff_pfp;
-      return param_5;
+      return p2_set1;
     }
   }
   fp = unaff_pfp;
